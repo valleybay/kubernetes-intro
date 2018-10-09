bc01d4a1db210524fea21832cdd1641336cada7f
valleybay <christian@ddev.no>
2018-10-09 09:28:18 +0200

    Changed some data

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Christian Dalsvaag", "location": "Oslo"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2013", "yearTo": "2018", "place": "Delta Dev", "comment": "Writing code"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
